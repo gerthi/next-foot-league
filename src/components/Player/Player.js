50cc9668defc1e873f3286cd683374d9e7361af6
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-11-14 14:51:42 +0100

    player helper done & pool`s orderBy working

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,49 +1,24 @@
 import styles from "./Player.module.scss";
 
-const teamParser = (team) => {
-  if (team === "Stade Brestois 29") {
-    return "Brest";
-  }
-  if (team === "Paris Saint Germain") {
-    return "PSG";
-  } else return team;
-};
-
 export default function Player({ player }) {
   return (
     <>
       <div className={styles.player}>
         <div className={styles.player_name}>{player.name}</div>
-        <div className={styles.player_club}>{teamParser(player.team)}</div>
-        <div className={styles.player_position}>
-          {player.position.slice(0, 3).toUpperCase()}
-        </div>
-        <div className={styles.player_minutes}>
-          {player.games.minutes_played > 0
-            ? Math.floor(player.games.minutes_played / player.games.appearences)
-            : 0}
-        </div>
-        <div className={styles.player_goals}>{player.goals.total}</div>
-        <div className={styles.player_assists}>{player.goals.assists}</div>
-        <div className={styles.player_assists}>{player.passes.key}</div>
-        <div className={styles.player_saves}>
-          {player.saves ? player.saves : "0"}
-        </div>
+        <div className={styles.player_club}>{player.club}</div>
+        <div className={styles.player_position}>{player.position}</div>
+        <div className={styles.player_minutes}>{player.minutes}</div>
+        <div className={styles.player_goals}>{player.goals}</div>
+        <div className={styles.player_assists}>{player.assists}</div>
+        <div className={styles.player_assists}>{player.keys}</div>
+        <div className={styles.player_saves}>{player.saves}</div>
         <div className={styles.player_interceptions}>
-          {player.tackles.interceptions}
+          {player.interceptions}
         </div>
-        <div className={styles.player_fouls}>{player.fouls.committed}</div>
-        <div className={styles.player_wonDuels}>
-          {player.duels.won
-            ? Math.floor((player.duels.won / player.duels.total) * 100) + "%"
-            : "0%"}
-        </div>
-        <div className={styles.player_passAcc}>{player.passes.accuracy}%</div>
-        <div className={styles.player_shotAcc}>
-          {player.shots.on
-            ? Math.floor((player.shots.on / player.shots.total) * 100) + "%"
-            : "0%"}
-        </div>
+        <div className={styles.player_fouls}>{player.fouls}</div>
+        <div className={styles.player_wonDuels}>{player.duels}%</div>
+        <div className={styles.player_passAcc}>{player.pAcc}%</div>
+        <div className={styles.player_shotAcc}>{player.sAcc}%</div>
       </div>
     </>
   );
--- src/components/PlayerPool/PlayerPool.js
+++ src/components/PlayerPool/PlayerPool.js
@@ -1,35 +1,56 @@
 import { useState } from "react";
 import Player from "../Player/Player";
 import styles from "./PlayerPool.module.scss";
+import {
+  KeyboardArrowDownRounded,
+  KeyboardArrowUpRounded,
+} from "@material-ui/icons";
 
 const orderBy = (players, value, direction) => {
   if (direction === "asc") {
-    return [...players].sort((a, b) => (a.value - b.value ? 1 : -1));
+    return [...players].sort((a, b) => (a[value] > b[value] ? 1 : -1));
   }
   if (direction === "desc") {
-    return [...players].sort((a, b) => (a.value - b.value ? -1 : 1));
+    return [...players].sort((a, b) => (a[value] > b[value] ? -1 : 1));
   }
   return players;
+};
+
+const SortArrows = ({ direction }) => {
+  if (!direction) {
+    return <></>;
+  }
+
+  if (direction === "desc") {
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowDownRounded color='inherit' />
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowUpRounded color='inherit' />
+      </div>
+    );
+  }
 };
 
 export default function PlayerPool({ players }) {
   const [direction, setDirection] = useState();
   const [value, setValue] = useState();
+
   const orderedPlayers = orderBy(players, value, direction);
 
   const switchDirection = () => {
-    if (!direction) {
+    if (!direction || direction === "asc") {
       setDirection("desc");
     } else if (direction === "desc") {
       setDirection("asc");
-    } else {
-      setDirection(null);
     }
   };
 
   const setValueAndDirection = (value) => {
-    console.log("value is", value);
-    console.log("direction is", direction);
     switchDirection();
     setValue(value);
   };
@@ -40,35 +61,102 @@ export default function PlayerPool({ players }) {
       <p>Found {players.length} players</p>
       <div className={styles.pool_container}>
         <div className={styles.pool_header}>
-          <h3
-            className={styles.name}
+          <button
+            className={styles.header_button}
             onClick={() => setValueAndDirection("name")}
           >
-            Name
-          </h3>
-          <h3 className={styles.club}>Club</h3>
-          <h3 className={styles.position}>Position</h3>
-          <h3 className={styles.minutes}>Min/g</h3>
-          <h3
-            className={styles.goals}
+            <span className={styles.name}>Name</span>
+            {value === "name" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("club")}
+          >
+            <span className={styles.club}>Club</span>
+            {value === "club" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("position")}
+          >
+            <span className={styles.position}>Position</span>
+            {value === "position" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("minutes")}
+          >
+            <span className={styles.minutes}>Min/g</span>
+            {value === "minutes" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
             onClick={() => setValueAndDirection("goals")}
           >
-            Goals
-          </h3>
-          <h3 className={styles.assists}>Assists</h3>
-          <h3 className={styles.assists}>Key pass</h3>
-          <h3 className={styles.saves}>Saves</h3>
-          <h3 className={styles.interceptions}>Interceptions</h3>
-          <h3 className={styles.fouls}>Fouls</h3>
-          <h3 className={styles.wonDuels}>Duels won</h3>
-          <h3 className={styles.passAcc}>Pass %</h3>
-          <h3 className={styles.shotAcc}>Shots %</h3>
+            <span className={styles.goals}>Goals</span>
+            {value === "goals" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("assists")}
+          >
+            <span className={styles.assists}>Assists</span>
+            {value === "assists" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("keys")}
+          >
+            <span className={styles.assists}>Key pass</span>
+            {value === "keys" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("saves")}
+          >
+            <span className={styles.saves}>Saves</span>
+            {value === "saves" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("interceptions")}
+          >
+            <span className={styles.interceptions}>Interceptions</span>
+            {value === "interceptions" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("fouls")}
+          >
+            <span className={styles.fouls}>Fouls</span>
+            {value === "fouls" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("duels")}
+          >
+            <span className={styles.wonDuels}>Duels won</span>
+            {value === "duels" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("pAcc")}
+          >
+            <span className={styles.passAcc}>Pass %</span>
+            {value === "pAcc" && <SortArrows direction={direction} />}
+          </button>
+          <button
+            className={styles.header_button}
+            onClick={() => setValueAndDirection("sAcc")}
+          >
+            <span className={styles.shotAcc}>Shots %</span>
+            {value === "sAcc" && <SortArrows direction={direction} />}
+          </button>
         </div>
-
         <div className={styles.pool_players}>
           {orderedPlayers.length > 0
-            ? players.map((player) => (
-                <div className={styles.player}>
+            ? orderedPlayers.map((player) => (
+                <div className={styles.player} key={player.id}>
                   <Player player={player} key={player.id} />
                 </div>
               ))
